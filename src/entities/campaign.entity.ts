a00467ab842d646b90d40de8b316b87d13056c65
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2026-01-24 16:01:39 -0500

    feat: implement database entities and relationships ref #2

---
+++ src/entities/campaign.entity.ts
@@ -0,0 +1,31 @@
+import { User } from './user.entity';
+import { Message } from './message.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { Status } from 'src/common/enums/status.enum';
+
+@Entity('campania')
+export class Campaign {
+  @PrimaryGeneratedColumn({ name: 'idCampania' })
+  id: number;
+
+  @Column({ name: 'nombre', length: 200 })
+  name: string;
+
+  @Column({ name: 'fechaHoraProgramacion', type: 'datetime' })
+  scheduledAt: Date;
+
+  @Column({ name: 'estado', type: 'tinyint', width: 1, default: Status.ACTIVE })
+  status: Status;
+
+  // FK: idUsuario
+  @ManyToOne(() => User, (user) => user.campaigns)
+  @JoinColumn({ name: 'idUsuario' })
+  user: User;
+
+  @Column({ name: 'idUsuario' })
+  userId: number;
+
+  // Relación: Una campaña tiene muchos mensajes
+  @OneToMany(() => Message, (message) => message.campaign)
+  messages: Message[];
+}
